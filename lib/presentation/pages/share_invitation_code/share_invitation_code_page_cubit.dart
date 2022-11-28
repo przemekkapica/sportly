19e438b98ed29713b9a604c505abf93f6d9d1a85
przemekkapica <przemekapica@gmail.com>
2022-11-28 19:58:31 +0100

    feat: Copy id token to clipboard (temp)

--- lib/presentation/pages/share_invitation_code/share_invitation_code_page_cubit.dart
+++ lib/presentation/pages/share_invitation_code/share_invitation_code_page_cubit.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+import 'package:sportly/domain/use_cases/get_id_token_use_case.dart';
 import 'package:sportly/presentation/pages/share_invitation_code/share_invitation_code_page_state.f.dart';
 
 @injectable
 class ShareInvitationPageCubit extends Cubit<ShareInvitationCodePageState> {
-  ShareInvitationPageCubit()
-      : super(const ShareInvitationCodePageState.loading());
+  ShareInvitationPageCubit(
+    this._getIdTokenUseCase,
+  ) : super(const ShareInvitationCodePageState.loading());
 
+  final GetIdTokenUseCase _getIdTokenUseCase;
   late final String _generatedCode;
 
   Future<void> init() async {
@@ -25,7 +28,9 @@ class ShareInvitationPageCubit extends Cubit<ShareInvitationCodePageState> {
   }
 
   Future<void> copyToClipboard() async {
-    ClipboardData data = ClipboardData(text: _generatedCode);
+    // ClipboardData data = ClipboardData(text: _generatedCode);
+    final token = await _getIdTokenUseCase();
+    ClipboardData data = ClipboardData(text: token);
 
     await Clipboard.setData(data);
   }
