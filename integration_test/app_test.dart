79f0d41b492e2963039dac2829eddea0c521fb82
przemekkapica <przemekapica@gmail.com>
2023-01-05 08:46:31 +0100

    chore: Update readme

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,8 +7,8 @@ import 'package:sportly/main.dart' as app;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('authentication flow', () {
-    testWidgets('signs in and out', (tester) async {
+  group('basic flow', () {
+    testWidgets('signs in', (tester) async {
       app.main();
 
       await tester.pumpAndSettle();
@@ -18,12 +18,42 @@ void main() {
       await tester.tap(signInButton);
       await tester.pumpAndSettle();
 
+      expect(find.text('Teams'), findsOneWidget);
       expect(find.text('Join a team'), findsOneWidget);
+      expect(find.text('Create a team'), findsOneWidget);
+
       final Finder profileButton = find.byIcon(Icons.account_circle);
       await tester.tap(profileButton);
       await tester.pumpAndSettle();
 
       expect(find.text('Sign out'), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      final Finder chatButton = find.byIcon(Icons.chat);
+      await tester.tap(chatButton);
+      await tester.pumpAndSettle();
+
+      expect(find.text('Chat'), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      final Finder scheduleButton = find.byIcon(Icons.calendar_month_rounded);
+      await tester.tap(scheduleButton);
+      await tester.pumpAndSettle();
+
+      expect(find.text('Schedule'), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(profileButton);
+      await tester.pumpAndSettle();
+
+      expect(find.text('Profile'), findsOneWidget);
+      expect(find.text('Sign out'), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
       final Finder signOutButton = find.byKey(const Key('sign-out-button'));
       await tester.tap(signOutButton);
       await tester.pumpAndSettle();
